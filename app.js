d016811ee70f881d2df87a8b5695a1a9d6b4f91f
sultan beddah <sultanbeddah@gmail.com>
2022-04-21 21:35:14 +0300

    gg

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // Your code goes here
+
+"use strict";
+const excelToJson = require("convert-excel-to-json");
+
+const result = excelToJson({
+  sourceFile: "names.xlsx",
+});
+
+//var moment = require("moment");
+let date = Date();
+
+console.log(result);
+document.getElementById("date").innerHTML = date;
+
+//sendEmail();
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "sender@email_address.com",
+//     Password: "Enter your password",
+//     To: "receiver@email_address.com",
+//     From: "sender@email_address.com",
+//     Subject: "Sending Email using javascript",
+//     Body: "Well that was easy!!",
+//     Attachments: [
+//       {
+//         name: "File_Name_with_Extension",
+//         path: "Full Path of the file",
+//       },
+//     ],
+//   }).then(function (message) {
+//     alert("Mail has been sent successfully");
+//   });
+// }
